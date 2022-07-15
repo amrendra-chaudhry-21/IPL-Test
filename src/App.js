837a493a9f9b79bddab655f13b222e735e4cec94
amrendrait43@gmail.com <Nh2conh2@aa432>
2022-07-15 15:01:22 +0530

    Medisage-test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import TeamMatches from './components/TeamMatches'
+import NotFound from './components/NotFound'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Switch>
+)
+export default App
